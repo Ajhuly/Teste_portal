68840964dac5b0847ee701f506f44578e6c91093
Ajhuly <julianar_s@yahoo.com.br>
2025-09-29 10:20:02 -0300

    Atualiza rotas e templates do portal

--- portal/routes.py
+++ portal/routes.py
@@ -5,3 +5,7 @@ from portal import app
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/sobre/quem-somos')
+def quem_somos():
+    return render_template('quem_somos.html')
